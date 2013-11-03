9bf014f5fe8d6b0f7a3176b32c510e17bc28bd7f
Brendan MacDonell <brendan@macdonell.net>
2013-11-03 18:49:35 -0500

    Render an appropriate CSS class for all flash types

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,18 @@
 module ApplicationHelper
+  # Returns a bootstrap alert CSS class appropriate for a flash level
+  # @author Brendan MacDonell
+  FLASH_CLASSES = {
+    :notice => "alert-info",
+    :success => "alert-success",
+    :alert => "alert-warning",
+    :error => "alert-danger",
+  }
+  FLASH_CLASSES.default = "alert-info"
+
+  def flash_class(level)
+    FLASH_CLASSES[level]
+  end
+
   # Helper that outputs "active" if a controller is active, or an
   # empty string otherwise. Used to flag navigation links active
   # when the controller they correspond to is visited.
