0ae98a93f698a61db92fadbbef172353ac5e1d22
Brendan MacDonell <brendan@macdonell.net>
2013-11-10 16:02:18 -0500

    Add system to render available actions. See state_action_renderable

---
+++ app/policies/proposal_policy.rb
@@ -0,0 +1,36 @@
+# @author Brendan MacDonell
+class ProposalPolicy < ApplicationPolicy
+
+  def show?
+    true
+  end
+
+  def submit?
+    user_member_of_project?
+  end
+
+  def accept?
+    user_supervises_project?
+  end
+
+  def return?
+    user_supervises_project?
+  end
+
+  private
+
+  alias_method :taskable, :record
+
+  def project
+    taskable.project
+  end
+
+  def user_supervises_project?
+    project.supervisors.include?(user)
+  end
+
+  def user_member_of_project?
+    project.group_members.include?(user)
+  end
+
+end
